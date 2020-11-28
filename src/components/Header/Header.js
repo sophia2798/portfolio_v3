37a95839320c958a3f7c787badd954422f28b6b5
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-27 21:23:55 -0800

    installed typing npm package

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./Header.css";
+import Typing from "react-typing-animation";
 
 function Header() {
+    const TypingComponent = () => (
+        <Typing speed={100}>
+            <h1>WELCOME! <br/> MY NAME IS SOPHIA JUNG</h1>
+        </Typing>
+    );
+
     return (
         <div className="header-container">
-            
+            <TypingComponent/>
         </div>
     )
 }
