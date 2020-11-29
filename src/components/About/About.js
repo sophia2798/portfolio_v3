bdecb55455160ed7f11fc35b1cdac8a2dc5add8a
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-28 20:34:37 -0800

    added bottom navbar to mobile screens

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ function About() {
                 <img className="bio-pic" src={bioImg} alt="bio picture"/>
                 <div className="bio-content">
                     <h1>HELLO, NICE TO MEET YOU!</h1>
-                    <p>I am a full-stack web developer with experience managing project teams and GitHub work flow. I am a 2020 graduate from the University of Washington with a B.S. in Applied Physics. I am comfortable with being assigned either back-end or front-end work and always welcome the opportunity to learn from any challenge!<br/><br/>If I'm not coding, you can find me with my nose in a good book, spending time with my dogs, listening to music, playing the piano, or tinkering with my PC build!<br/><br/>I am passionate about working with people and would love to help you turn your next big idea into a reality!</p>
+                    <p>I'm Sophia and I am a full-stack web developer with experience managing project teams and GitHub work flow. I graduated from the University of Washington in 2020 with a B.S. in Applied Physics. I am comfortable with both back-end and front-end work and always welcome the opportunity to learn from any challenge!<br/><br/>If I'm not coding, you can find me with my nose in a good book, spending time with my dogs, listening to music, playing the piano, or tinkering with my PC build!<br/><br/>I am passionate about working with people and would love to help you turn your next big idea into a reality!</p>
                 </div>
             </section>
             <div className="blend">
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUser, faFileAlt, faComment, faFolderOpen} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
     const [show, setShow] = useState(false);
@@ -39,19 +41,19 @@ function Navbar() {
             </div>
             <div className="mobile-nav">
                 <div className="mobile-nav-option">
-                    <Link to="#">HOME</Link>
+                    <HashLink to="/#header-hash" style={{display:'flex',flexDirection:'column'}}><span><FontAwesomeIcon className="mobile-icon" icon={faHome} /></span>HOME</HashLink>
                 </div>
                 <div className="mobile-nav-option">
-                    <Link to="#">ABOUT</Link>
+                    <HashLink to="/#about-hash" style={{display:'flex',flexDirection:'column'}}><span><FontAwesomeIcon className="mobile-icon" icon={faUser} /></span>ABOUT</HashLink>
                 </div>
                 <div className="mobile-nav-option">
-                    <Link to="#">PORTFOLIO</Link>
+                    <HashLink to="/#portfolio-hash" style={{display:'flex',flexDirection:'column'}}><span><FontAwesomeIcon className="mobile-icon" icon={faFolderOpen} /></span>PORTFOLIO</HashLink>
                 </div>
                 <div className="mobile-nav-option">
-                    <Link to="#">RESUME</Link>
+                    <Link to="#" style={{display:'flex',flexDirection:'column'}}><span><FontAwesomeIcon className="mobile-icon" icon={faFileAlt} /></span>RESUME</Link>
                 </div>
                 <div className="mobile-nav-option">
-                    <Link to="#">CONTACT</Link>
+                    <Link to="#" style={{display:'flex',flexDirection:'column'}}><span><FontAwesomeIcon className="mobile-icon" icon={faComment} /></span>CONTACT</Link>
                 </div>
             </div>
         </div>
