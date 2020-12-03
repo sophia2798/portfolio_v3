7a43d1288328414284d0e464bbb67baa4e254a95
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-12-02 18:15:01 -0800

    minor styling changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
                 <a target="_blank" href="https://www.linkedin.com/in/sophia2798/" className="contact-links"><FontAwesomeIcon className="contact-icon" id="linkedin-icon" icon={faLinkedin}/></a>
                 <Link to="/contact" className="contact-links"><FontAwesomeIcon className="contact-icon" icon={faEnvelope}/></Link>
             </div>
-            <p id="footer-message" style={{marginBottom: '2.5rem'}}>VISIT MY SOCIALS TO LEARN MORE ABOUT ME OR TO START A CONVERSATION! I CAN'T WAIT TO HEAR FROM YOU!</p>
+            <p id="footer-message" style={{marginBottom: '2.5rem'}}>VISIT MY SOCIALS TO LEARN MORE ABOUT ME OR TO START A CONVERSATION</p>
             <p style={{fontWeight:'bold', letterSpacing:'0.15rem'}}>SOPHIA JUNG</p>
             <p style={{marginTop: '1rem'}}><small> &#169; COPYRIGHT 2020</small></p>
             <div style={{display:'flex', justifyContent:'center', marginTop:'0.4rem'}}>
