818f5d96acae8a4c1f6ce17408503c1b13d58dfb
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-12-02 19:04:29 -0800

    minor mobile responsiveness styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,6 +8,7 @@ function About() {
     return (
         <div className="about-container" id="about-hash">
             <section className="short-bio">
+                <div className="bio-content-container">
                 <img className="bio-pic" src={bioImg} alt="bio picture"/>
                 <div className="bio-content">
                     <h1><span style={{color:'white'}}><span className="tag">&lt;</span> DEVELOPER <span className="tag">&gt;</span></span> <span style={{color:'white'}}><span className="tag">&lt;</span> DESIGNER <span className="tag">&gt;</span></span> <span style={{color:'white'}}><span className="tag">&lt;</span> DREAMER <span className="tag">&gt;</span></span></h1>
@@ -19,6 +20,7 @@ function About() {
                         <span style={{color:'white'}}><span className="tag">&lt;</span> DREAMER <span className="tag">&gt;</span></span>
                     </h1>
                     <p>Hello, nice to meet you! I'm Sophia and I am a full-stack web developer with experience managing project teams and GitHub work flow. I graduated from the University of Washington in 2020 with a B.S. in Applied Physics. I am comfortable with both back-end and front-end work and always welcome the opportunity to learn from any challenge!<br/><br/>If I'm not coding, you can find me with my nose in a good book, spending time with my dogs, listening to music, playing the piano, or tinkering with my PC build!<br/><br/>I am passionate about working with people and would love to help you turn your next big idea into a reality!</p>
+                </div>
                 </div>
             </section>
             <div className="blend">
