b6b260646ca5f1c4a3ea4d6eac17c4d56dffad2a
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-12-04 00:45:00 -0800

    finished styling portfolio cards, mobile responsiveness

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -9,13 +9,21 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import icon from "../../media/guardian-logo.jpeg"
+import guardianicon from "../../media/guardian-logo.jpeg"
+import petworkicon from "../../media/petwork-logo.png"
+import covidicon from "../../media/covid-logo.png"
+import employeeicon from "../../media/employee-logo.png"
+import guardianscreen from "../../media/guardian-screenshot.png"
+import petworkscreen from "../../media/petwork-screenshot.png"
+import employeescreen from "../../media/employee-screenshot.png"
+import covidscreen from "../../media/covid-screenshot.png"
 import "./Cards.scss";
 
 const useStyles = makeStyles((theme) => ({
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
+    filter: 'grayscale(50%)'
   },
 }));
 
@@ -25,18 +33,35 @@ function Cards() {
   const projects = [
       {
           title: "GUARDIAN",
-          information: "Application for EP industry",
-          id: 1
+          information: "A fullstack application geared towards the Executive Protection Industry. Guardian allows EP teams to coordinate missions by allowing management and agents on the ground to stay up to date with mission information. Users can view all upcoming missions and the pertinent information for each mission. This includes, weather, an embedded map with pins at each location on the mission's iterinary, and a whiteboard for team members to leave notes",
+          tools: "Third Party APIs, CORS, MongoDB, Mongoose, JWT, Node.js, Express.js, React.js, Material-UI, react-google-maps, react-date-range, bcrypt, dotenv",
+          id: 1,
+          icon: guardianicon,
+          screenshot: guardianscreen,
       },
       {
           title: "THE SOCIAL PETWORK",
-          information: "Social media for pets",
-          id: 2
+          information: "A fullstack application centered around bringing pets and their owners together. This social media-like application allows users to add their pets to a profile with generic information such as, age, breed, and hobbies. Other users can view this information to help decide whether they think another pet would be a good match for their pet. Users can also input their location which allows them to view pets in their area. Users can favorite pets and directly contact the owners to set up a playdate.",
+          tools: "TOOLS: Third Party APIs, Handlebars, Express.js, Node.js, bcrypt, Express Sessions, MySQL, MySQL Workbench, Postman, Cloudinary, Sequelize, dotenv",
+          id: 2,
+          icon: petworkicon,
+          screenshot: petworkscreen,
       },
       {
           title: "COVID-19 STATISTICS",
-          information: "Page for local COVID-19 data",
-          id: 3
+          information: "A predominantly front-end application that utilizes COVID statistics and geocoding API's to provide the user with current data on the COVID situation in a given area. Users can input their desired city and state code and view general statistics, nearby testing centers, and the contact information for the local health department. Additionally, users can also view a graphical representation of the COVID statistics over time. The searched cities are saved in a log which can be used to quickly view COVID information for previously searched locations. The searched cities log can also be cleared at any time.",
+          tools: "Third Party APIs, jQuery, Chart.js, Moment.js, Materialize",
+          id: 3,
+          icon: covidicon,
+          screenshot: covidscreen,
+      },
+      {
+          title: "EMPLOYEE DIRECTORY",
+          information: "This React.js application organizes a large directory of employees. The database of employees is retrieved from Random User API. The application uses React lifecycle methods to allow users to filter the directory by first name, last name, or date of birth. The user is also able to search for an employee's name in the search bar. The search bar also automatically provides a dropdown menu with the all the employee's name and autocomplete funcionality to help the user narrow down their search.",
+          tools: "TOOLS: Third Party APIs, React.js, Axios, Moment.js",
+          id: 4,
+          icon: employeeicon,
+          screenshot: employeescreen,
       },
   ]
 
@@ -47,12 +72,12 @@ function Cards() {
   return (
     <div className="cards">
         {projects.map((project, i) => (
-            <div className="test">
+            <div className="map-card">
             <Card className="portfolio-card" key={project.id}>
             <CardHeader
             avatar={
                 <Avatar aria-label="recipe" style={{backgroundColor:'black'}}>
-                <img className="icon-img" src={icon} alt="project-logo"/>
+                <img className="icon-img" src={project.icon} alt="project-logo"/>
                 </Avatar>
             }
             action={
@@ -69,14 +94,21 @@ function Cards() {
             />
             <CardMedia
             className={classes.media}
-            image="https://t4.ftcdn.net/jpg/02/91/24/27/360_F_291242770_z3XC7rJB1Mvc5jVMsEY9Dx2xMrX4sxUi.jpg"
+            image={project.screenshot}
             title="Project Image"
             />
             <CardContent>
             </CardContent>
             <Collapse in={expanded === i} timeout="auto" unmountOnExit>
             <CardContent>
-                <Typography className="expand-info" paragraph>ACTIVITIES LIST</Typography>
+                <Typography className="expand-info" paragraph>
+                    <h3>DESCRIPTION</h3>
+                    {project.information}
+                </Typography>
+                <Typography className="expand-info" paragraph>
+                    <h3>TOOLS</h3>
+                    {project.tools}
+                </Typography>
             </CardContent>
             </Collapse>
         </Card>
